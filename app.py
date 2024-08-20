9db6243ebb0c87e1eab263e321cfca26530a70f8
ranv33r07 <ranveer072002@gmail.com>
2024-08-20 21:15:13 +0530

    Modified the routes

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify
-from database import engine,load_jobs_from_db
+from database import engine,load_jobs_from_db,load_job_from_db
 
 app = Flask(__name__) 
 
@@ -8,11 +8,15 @@ def hello_World():
     return render_template("main.html", jobs=load_jobs_from_db())
 
 @app.route("/jobs/")
+def get_jobs():
+    jobs = load_jobs_from_db()
+    return jsonify(jobs)
 
-@app.route("/api/jobs")
-def jobs():
-    return jsonify(load_jobs_from_db())
-
+@app.route("/jobs/<id>")
+def get_job(id):
+    job = load_job_from_db(id)
+    if (job == None) : return jsonify("No record Found" )
+    return jsonify(job)
 
 print(__name__)
 if __name__ == "__main__":
--- database.py
+++ database.py
@@ -2,7 +2,7 @@ import sqlalchemy
 from sqlalchemy import create_engine, text
 import os
 
-db_connection_string = os.environ['DB_CONNECTION_STRING']
+db_connection_string = "mysql+pymysql://2MV7kAJ8u835cqi.root:qOjWOWxOhLy0Zi86@gateway01.ap-southeast-1.prod.aws.tidbcloud.com/test?charset=utf8mb4"
 
 engine = create_engine(db_connection_string,
                        connect_args={"ssl": {
@@ -12,9 +12,16 @@ engine = create_engine(db_connection_string,
 def load_jobs_from_db():
     with engine.connect() as conn:
         result = conn.execution_options(stream_results=True).execute(
-            text("select * from jobs"))
+            text("SELECT * FROM jobs")
+        )
+        jobs = [dict(row._mapping) for row in result]
+    return jobs
 
-        JOBS = []
-        for row in result.all():
-            JOBS.append(row)
-    return JOBS
+def load_job_from_db(job_id):
+    with engine.connect() as conn:
+        result = conn.execute(
+            text("SELECT * FROM jobs WHERE id = :id"), {"id": job_id}
+        )
+    rows = result.all()
+    if (len(rows) == 0) : return None
+    else : return dict(rows[0]._mapping)
