17858d0c3578d1fb3fae353329cb8f90533c9099
ranv33r07 <ranveer072002@gmail.com>
2024-08-21 15:40:20 +0530

    Updated and added application feature

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, jsonify
-from database import engine,load_jobs_from_db,load_job_from_db
+from flask import Flask, render_template, jsonify,request
+from database import engine,load_jobs_from_db,load_job_from_db,add_applications_to_db
 
 app = Flask(__name__) 
 
@@ -16,8 +16,16 @@ def get_jobs():
 def get_job(id):
     job = load_job_from_db(id)
     if (job == None) : return jsonify("No record Found" )
-    return jsonify(job)
+    return render_template("jobpage.html" , j=job)
 
-print(__name__)
+@app.route("/jobs/<id>/apply" , methods = ["post"])
+def apply_to_job(id):
+    data = request.form
+    job = load_job_from_db(id)
+    add_applications_to_db(id , data)
+    return render_template("application_submitted.html",data = data,job = job)
+
+    
+
 if __name__ == "__main__":
     app.run(debug = True)
--- database.py
+++ database.py
@@ -2,7 +2,7 @@ import sqlalchemy
 from sqlalchemy import create_engine, text
 import os
 
-db_connection_string = "mysql+pymysql://2MV7kAJ8u835cqi.root:qOjWOWxOhLy0Zi86@gateway01.ap-southeast-1.prod.aws.tidbcloud.com/test?charset=utf8mb4"
+db_connection_string = "mysql+pymysql://2MV7kAJ8u835cqi.root:qOjWOWxOhLy0Zi86@gateway01.ap-southeast-1.prod.aws.tidbcloud.com/test?charset=utf8mb4" 
 
 engine = create_engine(db_connection_string,
                        connect_args={"ssl": {
@@ -25,3 +25,23 @@ def load_job_from_db(job_id):
     rows = result.all()
     if (len(rows) == 0) : return None
     else : return dict(rows[0]._mapping)
+
+
+def add_applications_to_db(id, data):
+    with engine.begin() as conn:
+        query = text("""
+            INSERT INTO applications 
+            (job_id, full_name, email, linkedin, education, experience, resume) 
+            VALUES (:job_id, :full_name, :email, :linkedin, :education, :experience, :resume)
+        """)
+        conn.execute(query, {
+            "job_id": id,
+            "full_name": data['Name'],
+            "email": data["Email"],
+            "linkedin": data["linkedin"],
+            "education": data["education"],
+            "experience": data["experience"],
+            "resume": data["resume"]
+        })
+        print("Data inserted with transaction block.")
+
